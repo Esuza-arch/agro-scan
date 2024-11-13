471ec930dcd1875ab808beb6cb33cfadf2e1c6a5
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-13 06:06:28 +0300

    import Home and Search components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Search from './components/Search'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <Search />
     </div>
   )
 }
