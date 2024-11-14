6b41725189d3b0b2e1dbceab1d2c164de216f012
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-14 16:22:05 +0300

    add preventDefault and input

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react'
 
 function Search(setSearchResults) {
   const [searchTerm, setSearchTerm] = useState('')
-  const handleSearch = () => {
+  
+  const handleSearch = (e) => {
+    e.preventDefault()
     fetch(`http://localhost:3001/products?name_like=${searchTerm}`)
       .then((res) => res.json())
       .then((data) => setSearchResults(data))
   }
   return (
     <form>
+      <input type='text' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder='Search product by name' />
       <button onClick={handleSearch}>Search</button>
     </form>
   )
