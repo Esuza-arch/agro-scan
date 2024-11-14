080521efd1e075319d5d95fac081809c1b097b8f
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-14 16:15:01 +0300

    add fetch

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 
-function Search() {
+function Search(setSearchResults) {
   const [searchTerm, setSearchTerm] = useState('')
   const handleSearch = () => {
-    fetch(``)
+    fetch(`http://localhost:3001/products?name_like=${searchTerm}`)
+      .then((res) => res.json())
+      .then((data) => setSearchResults(data))
   }
   return (
-    <div>Search</div>
+    <div>
+      Search
+    </div>
   )
 }
 
