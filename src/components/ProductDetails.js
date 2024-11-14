70bbf84f3a28dada6ed6ffdcc078172cbaecf1fa
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-14 22:36:22 +0300

    add new const to ProductDetails function

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-function ProductDetails() {
+function ProductDetails({ products }) {
+  const { id } = useParams()
+  const product = products.find((p) => p.id === parseInt(id))
+
+  if (!product) return <p>Product not found</p>
+
   return (
     <div>ProductDetails</div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,8 @@ function Search(setSearchResults) {
     fetch(`http://localhost:3001/products?name_like=${searchTerm}`)
       .then((res) => res.json())
       .then((data) => setSearchResults(data))
-  }
+      .catch((error) => console.error("Error fetching product:", error))
+  } 
   return (
     <form>
       <input type='text' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder='Search product by name' />
