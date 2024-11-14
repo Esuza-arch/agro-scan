62a9ac86ac32e5ce7cc77f5f03dc15df5121c8a2
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-14 16:49:46 +0300

    add useEfect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Search from './components/Search'
 
 function App() {
+  const [products, setProducts] = useState([])
   const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+  }, [])
   return (
     <div>
       <NavBar />
       <Home />
-      <Search />
+      <Search setSearchResults={setSearchResults} />
     </div>
   )
 }
