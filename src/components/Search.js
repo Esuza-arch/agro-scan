9de9d02f43d099d53a6413fadd7359ffeabb8702
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-15 17:19:44 +0300

    add handleSubmit func to form

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,7 +11,7 @@ function Search() {
   return (
     <div>
       <h2>Search Products</h2>
-      <form>
+      <form onSubmit={handleSubmit} >
         <input type='text' placeholder='Enter product name or code...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
         <button type='submit'>Search</button>
       </form>
