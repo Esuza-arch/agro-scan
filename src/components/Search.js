2bf7db635a55fc3e933fb0eb0dc47756644a506b
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-15 17:11:59 +0300

    add handleSubmit to App

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react'
 function Search() {
   const [searchTerm, setSearchTerm] = useState('')
   
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSearch(searchTerm)
+  }
+  
   return (
     <form>
       <input type='text' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder='Search product by name' />
