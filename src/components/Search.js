4be9dfa9d96d673a7b2f3cfba9fe1b184aff407a
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-15 17:16:53 +0300

    add new form

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,6 +11,9 @@ function Search() {
   return (
     <div>
       <h2>Search Products</h2>
+      <form>
+        <input type='text' placeholder='Enter product name or code...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      </form>
     </div>
   )
 }
