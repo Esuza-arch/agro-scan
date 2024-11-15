9f5be0b35efbc88967d27f34486aea14b99a284d
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-15 18:48:48 +0300

    add className to div in Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,7 @@ function Search({ onSearch }) {
   }
 
   return (
-    <div>
+    <div className='search'>
       <h2>Search Products</h2>
       <form onSubmit={handleSubmit} >
         <input type='text' placeholder='Enter product name or code...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
