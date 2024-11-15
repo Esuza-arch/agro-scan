3aace27b87f30efcc6b0b538b0d661c00bea3b2a
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-15 16:24:57 +0300

    add new div and h3 elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,12 @@
      <div>
         <h1>Welcome to AgroScan Database</h1>
         <p>Your comprehensive resource for agricultural chemicals information.</p>
+        <div>
+          <div>
+            <h3>Search Products</h3>
+            <p>Find detailed information about fertilizers and pesticides</p>
+          </div>
+        </div>
      </div>
    )
  }
